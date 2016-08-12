711255a2b805468dd2aa331c477ce8cafb110c17
caowei <caowei@zenmen.com>
2016-08-12 20:00:32 +0800

    first commit

---
+++ app/src/main/java/com/tony/timemanager/base/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.tony.timemanager.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tony on 8/12/16.
+ */
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/tony/timemanager/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tony.timemanager.ui;
+
+import android.os.Bundle;
+
+import com.tony.timemanager.R;
+import com.tony.timemanager.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
